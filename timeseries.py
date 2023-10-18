42f15460d83ee3e1e84f542d0d441d941d16a890
sm4l <86418689+sm4l@users.noreply.github.com>
2023-10-18 11:13:47 -0300

    comit do ts que estava no note

--- timeseries.py
+++ timeseries.py
@@ -3,6 +3,8 @@ import time
 import datetime
 import sys
 import logging
+import logging
+import sys
 
 # Configurar o logger para escrever em um arquivo de log
 logging.basicConfig(filename='log.txt', level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
@@ -14,6 +16,9 @@ console_handler.setLevel(logging.INFO)  # Definir o nível de severidade desejad
 # Adicionar o manipulador ao logger raiz
 logger = logging.getLogger()
 logger.addHandler(console_handler)
+
+#sys.stdout = open('/home/noteaeris/proj/logts.log', 'a')
+#sys.stderr = sys.stdout
 
 # Configurações do banco de dados MySQL
 db_host = "localhost"  # Altere para o host do seu banco de dados MySQL
@@ -34,24 +39,6 @@ def obter_ultimo_minuto_registrado(connection, topic):
     ultimo_minuto = cursor.fetchone()[0]
     cursor.close()
     return ultimo_minuto
-
-def verificar_e_inserir_dados(connection, topic, minuto_inteiro, data):
-    ultimo_minuto_registrado = obter_ultimo_minuto_registrado(connection, topic)
-
-    if ultimo_minuto_registrado is None or ultimo_minuto_registrado < minuto_inteiro:
-        cursor = connection.cursor()
-        media = calcular_media_por_minuto(data['valores'])
-        count = data['count']
-        query = "INSERT INTO {} (avg, min, max, cnt, topic, unit, timestamp) VALUES (%s, %s, %s, %s, %s, %s, %s)".format(timeseries_table_name)
-        values = (media, min(data['valores']), max(data['valores']), count, topic, data['unit'], minuto_inteiro)
-        cursor.execute(query, values)
-        cursor.close()
-        connection.commit()
-        #print(f"{hora} >>>>INSERIDO COM SUCESSO<<<<")
-        logger.debug(">>>INSERT OK<<<")
-    else:
-        print(f"{hora} Já existem registros para o minuto {minuto_inteiro}. Dados não serão duplicados.")
-        logger.debug(f"Já existem registros para o minuto {minuto_inteiro}. Dados não serão duplicados.")
 
 def ler_dados_do_banco(ultimo_minuto):
     # Estabelecer conexão com o banco de dados
@@ -85,25 +72,58 @@ def ler_dados_do_banco(ultimo_minuto):
 
     return dados_por_topico
 
+def escrever_dados_no_banco(dados_por_topico):
+    # Estabelecer conexão com o banco de dados
+    connection = mysql.connector.connect(host=db_host, user=db_user, password=db_password, database=db_name)
+    
+    for (topic, minuto_inteiro), data in dados_por_topico.items():
+        ultimo_minuto_registrado = obter_ultimo_minuto_registrado(connection, topic)
+        
+        # Verificar se já existem registros para o último minuto
+        if ultimo_minuto_registrado is not None and ultimo_minuto_registrado >= minuto_inteiro:
+            time.sleep(30)
+            print(f"{hora} Já existem registros para o minuto {minuto_inteiro}. Dados não serão duplicados.")
+            logger.debug(f"Já existem registros para o minuto {minuto_inteiro}. Dados não serão duplicados.")
+            continue
+        
+        cursor = connection.cursor()
+
+        media = calcular_media_por_minuto(data['valores'])
+        count = data['count']
+
+        # Consulta SQL para inserir os dados na tabela timeseries
+        query = "INSERT INTO {} (avg, min, max, cnt, topic, unit, timestamp) VALUES (%s, %s, %s, %s, %s, %s, %s)".format(timeseries_table_name)
+        values = (media, min(data['valores']), max(data['valores']), count, topic, data['unit'], minuto_inteiro)
+        cursor.execute(query, values)
+        cursor.close()
+
+    # Confirmar as alterações no banco de dados
+    connection.commit()
+
+    # Fechar a conexão com o banco de dados
+    connection.close()
+    print(str(hora) + " >>>>INSERIDO COM SUCESSO<<<<")
+    logger.debug(">>>INSERT OK<<<<")
+
+
 if __name__ == "__main__":
     # Definir o intervalo de tempo para o loop (por exemplo, a cada minuto)
     intervalo_minuto = 60
     last_logged_minute = None  # Variável para controlar o último minuto registrado
+ 
+
 
     while True:
         try:
             ultimo_minuto = int(time.time()) - (int(time.time()) % intervalo_minuto)
             unixt = datetime.datetime.now()
             hora = unixt.strftime('%Y-%m-%d %H:%M:%S')
-            print(f"{hora} Iniciando Ciclo")
+            print(str(hora) + " Iniciando Ciclo")
             logger.debug("Iniciando Ciclo")
 
             if ultimo_minuto != last_logged_minute:  # Verifique se há novos dados a serem processados
                 dados_por_topico = ler_dados_do_banco(ultimo_minuto)
-                connection = mysql.connector.connect(host=db_host, user=db_user, password=db_password, database=db_name)
-                for (topic, minuto_inteiro), data in dados_por_topico.items():
-                    verificar_e_inserir_dados(connection, topic, minuto_inteiro, data)
-                connection.close()
+                escrever_dados_no_banco(dados_por_topico)
                 last_logged_minute = ultimo_minuto
 
             time.sleep(intervalo_minuto)
